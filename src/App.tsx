4c1f0ae3195551efc4f32f3cdcd21a9fcfd9231e
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-02-12 23:17:19 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
